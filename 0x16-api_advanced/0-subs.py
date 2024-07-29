6405dc3ab8b11306e9507edac08e4c59648b85f8
njaup <njaup974@gmail.com>
2024-07-29 15:11:27 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,6 +2,7 @@
 """This module queries the Reddit API and returns the number of subscribers"""
 
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
@@ -16,5 +17,13 @@ def number_of_subscribers(subreddit):
             return data.get('data', {}).get('subscribers', 0)
         else:
             return 0
-    except requests.RequestException as e:
+    except requests.RequestException:
         return 0
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: ./0-main.py <subreddit>")
+    else:
+        subreddit = sys.argv[1]
+        print(f"Number of subscribers in subreddit '{subreddit}': {number_of_subscribers(subreddit)}")
