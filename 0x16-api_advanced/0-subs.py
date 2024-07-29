908b553bdd9eeee149cea3acfac199ba59e760f8
njaup <njaup974@gmail.com>
2024-07-29 15:24:34 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,7 +2,6 @@
 """This module queries the Reddit API and returns the number of subscribers"""
 
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
@@ -14,8 +13,8 @@ def number_of_subscribers(subreddit):
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
             data = response.json()
-            return(data.get('data', {}).get('subscribers', 0))
+            return data.get('data', {}).get('subscribers', 0)
         else:
-            return(0)
+            return 0
     except requests.RequestException:
-        return(0)
+        return 0
