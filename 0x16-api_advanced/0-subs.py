1260fd2a39eac806584d77db433e3437bcba526f
njaup <njaup974@gmail.com>
2024-07-29 15:32:52 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""This module queries the Reddit API and returns the number of subscribers"""
-
+"""Counts number of subs"""
 import requests
 
 
-def number_of_subscribers(subreddit):
-    """Returns the total number of subscribers for a given subreddit"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Python/requests"}
+def number_of_subscribers(subreddit=None):
+    """Function to retrieve the number of subscribers for a given subreddit"""
+
+    if not subreddit or not isinstance(subreddit, str):
+        return 0
 
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data.get('data', {}).get('subscribers', 0)
-        else:
-            return 0
-    except requests.RequestException:
+        result = requests.get('http://www.reddit.com/r/{}/about.json'.
+                              format(subreddit),
+                              headers={'User-Agent': 'mycoolapp/1.0'})
+        result.raise_for_status()
+        data = result.json()
+        subscribers = data.get("data", {}).get("subscribers", 0)
+        return subscribers
+    except Exception:
         return 0
