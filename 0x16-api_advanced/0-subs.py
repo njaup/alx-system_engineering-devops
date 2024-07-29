c1cabbc9b69d72ac9b844787f70dd837e7a5074e
njaup <njaup974@gmail.com>
2024-07-29 14:21:27 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,14 +4,18 @@
 import json
 import requests
 
-
 def number_of_subscribers(subreddit):
     """Returns the total number of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "Python/requests"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
+    headers = {"User-Agent": "my-custom-user-agent-v1.0"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException as e:
+        print(f"An error occurred: {e}")
         return 0
